78e0d19d2a5034c10e06bfaacf7b20b833b6bfe7
Zain Siddiqi <57043176+z-siddiqi@users.noreply.github.com>
2026-07-19 14:20:21 +0100

    remove unused spinner css

--- src/lib/components/PromptInput.svelte
+++ src/lib/components/PromptInput.svelte
@@ -679,16 +679,4 @@
     }
   }
 
-  .spinner {
-    animation: spin 1s linear infinite;
-  }
-
-  @keyframes spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
 </style>
